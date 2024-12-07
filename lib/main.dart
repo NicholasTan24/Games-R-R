50037c62f8a2d0551a15498f36a7189803fd73a8
Rafael <RafaelEbenHart_2327240120@mhs.mdp.ac.id>
2024-12-07 07:55:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:games_rr/data/games_data.dart';
+import 'package:games_rr/screens/HomeScreen.dart';
 import 'package:games_rr/screens/layar_detail.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,8 +13,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Games R-R',
-      home: DetailScreen(games: gameList[0]),
+      title: 'Games-R-R',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.transparent,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home:Gradient(child: Homescreen()),
+    );
+  }
+}
+class Gradient extends StatelessWidget {
+  final Widget child;
+  const Gradient({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Color(0xff1E3E62),Colors.white,],
+              begin: Alignment.topCenter,
+              end: Alignment(0,10)
+          ),
+        ),
+        child: child,
+      ),
     );
   }
 }
